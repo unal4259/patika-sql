3a2d50581bde5e75582a49266fe3184083416d9a
unalgecer <unalg4259@gmail.com>
2021-11-24 15:01:18 +0300

    ödev-2 first commit

---
+++ ödev-2.sql
@@ -0,0 +1,26 @@
+-- film tablosunda bulunan tüm sütunlardaki verileri replacement cost değeri 12.99 dan büyük eşit ve 16.99 küçük olma 
+-- koşuluyla sıralayınız.
+
+SELECT replacement_cost
+FROM film
+WHERE replacement_cost BETWEEN 12.99 AND 16.98
+ORDER BY replacement_cost;
+
+/*SELECT replacement_cost
+FROM film
+WHERE replacement_cost >= 12.99 AND replacement_cost < 16.99
+ORDER BY replacement_cost;*/
+
+-- actor tablosunda bulunan first_name ve last_name sütunlardaki verileri first_name 'Penelope' veya 'Nick' veya 'Ed' 
+-- değerleri olması koşuluyla sıralayınız. 
+SELECT first_name,last_name
+FROM actor
+WHERE first_name IN ('Penelope','Nick','Ed')
+ORDER BY first_name;
+
+-- film tablosunda bulunan tüm sütunlardaki verileri rental_rate 0.99, 2.99, 4.99 VE replacement_cost 12.99, 15.99, 28.99 olma 
+-- koşullarıyla sıralayınız.
+SELECT * 
+FROM film
+WHERE (rental_rate IN (0.99,2.99,4.99)) AND (replacement_cost IN(12.99,15.99,28.99))
+ORDER BY rental_rate;
